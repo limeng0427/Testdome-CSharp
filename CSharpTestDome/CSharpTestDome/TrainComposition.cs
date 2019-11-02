906e8b5c7ceb4b80e6591e7ff18abc486ef61c50
Liam <limeng0427@users.noreply.github.com>
2019-11-02 14:25:48 +1300

    Update TrainComposition.cs

--- CSharpTestDome/CSharpTestDome/TrainComposition.cs
+++ CSharpTestDome/CSharpTestDome/TrainComposition.cs
@@ -15,103 +15,29 @@ using System;
 
 public class TrainComposition
 {
-    public class TrainNode
-    {
-        public int Value;
-        public TrainNode LeftTrain;
-        public TrainNode RightTrain;
-
-        public TrainNode(int value)
-        {
-            this.Value = value;
-            this.LeftTrain = null;
-            this.RightTrain = null;
-        }
-
-        public void SetRightTrain(TrainNode right)
-        {
-            this.RightTrain = right;
-        }
-
-        public void SetLeftTrain(TrainNode left)
-        {
-            this.LeftTrain = left;
-        }
-    }
-
-    private TrainNode leftMost;
-    private TrainNode rightMost;
-
-    public TrainComposition()
-    {
-        leftMost = null;
-        rightMost = null;
-    }
-
+    public LinkedList<int> WagonList = new LinkedList<int>();
     public void AttachWagonFromLeft(int wagonId)
     {
-        var tmp = new TrainNode(wagonId);
-        if (leftMost != null)
-        {
-            // Trains in composition
-            leftMost.SetLeftTrain(tmp);
-            tmp.SetRightTrain(leftMost);
-            leftMost = tmp;
-        }
-        else
-        {
-            leftMost = tmp;
-            rightMost = tmp;
-        }
+        WagonList.AddLast(wagonId);
     }
 
     public void AttachWagonFromRight(int wagonId)
     {
-        var tmp = new TrainNode(wagonId);
-        if (rightMost != null)
-        {
-            // Trains in composition
-            rightMost.SetRightTrain(tmp);
-            tmp.SetLeftTrain(rightMost);
-            rightMost = tmp;
-        }
-        else
-        {
-            leftMost = tmp;
-            rightMost = tmp;
-        }
+        WagonList.AddFirst(wagonId);
     }
 
     public int DetachWagonFromLeft()
     {
-        TrainNode tmp;
-        if (leftMost != null)
-        {
-            tmp = leftMost;
-            leftMost = leftMost.RightTrain;
-            var tmpValue = tmp.Value;
-            return tmpValue;
-        }
-        else
-        {
-            return 0;
-        }
+        int result = WagonList.Last.Value;
+        WagonList.RemoveLast();
+        return result;
     }
 
     public int DetachWagonFromRight()
     {
-        TrainNode tmp;
-        if (rightMost != null)
-        {
-            tmp = rightMost;
-            rightMost = rightMost.LeftTrain;
-            var tmpValue = tmp.Value;
-            return tmpValue;
-        }
-        else
-        {
-            return 0;
-        }
+        int result = WagonList.First.Value;
+        WagonList.RemoveFirst();
+        return result;
     }
 
     public static void Main(string[] args)
