3f130b8b1f064103f43901bd698f3558968eb613
Liam <limeng0427@users.noreply.github.com>
2019-11-02 15:16:38 +1300

    Update Path.cs

--- CSharpTestDome/CSharpTestDome/Path.cs
+++ CSharpTestDome/CSharpTestDome/Path.cs
@@ -31,36 +31,31 @@ public class Path
 
     public void Cd(string newPath)
     {
-        if (newPath.StartsWith("/"))
+        var regex = new Regex(@"(..)?\/?([a-z,A-Z])*(\/[a-z,A-Z])*");
+        if (!regex.IsMatch(newPath))
+            return;
+        if (newPath[0] == '/')
         {
             CurrentPath = newPath;
         }
-        else if (newPath.Contains(".."))
-        {
-            var pathList = new LinkedList<string>(CurrentPath.Split('/')); // will get 5 elements, the first element is empty string
-            var newPathList = newPath.Split('/'); // will get two elements namely '..' and 'x'
-            foreach (var item in newPathList)
-            {
-                if (item == "..") // go one step back, so remove the last folder
-                {
-                    if (pathList.Count > 0)
-                        pathList.RemoveLast();
-                }
-                else
-                {
-                    // 'd' will be replaced by 'x', 'd' was the last element which was removed in the first iteration, 
-                    // now 'x' is added as the last element
-                    pathList.AddLast(item);
-                }
-            }
-
-            CurrentPath = string.Join("/", pathList); // set all items in the list to one string delimited by '/' ("/a/b/c/x")
-            if (!CurrentPath.StartsWith("/"))
-                CurrentPath = "/" + CurrentPath; // if the string does not start with '/' then add a '/' in the beginning of the string
-        }
         else
         {
-            CurrentPath += "/" + newPath;
+            LinkedList<string> folders = new LinkedList<string>(CurrentPath.Split('/'));
+            folders.RemoveFirst();
+            LinkedList<string> newFolders = new LinkedList<string>(newPath.Split('/'));
+            foreach (var item in newFolders)
+            {
+                switch (item)
+                {
+                    case "..":
+                        folders.RemoveLast();
+                        break;
+                    default:
+                        folders.AddLast(item);
+                        break;
+                }
+            }
+            CurrentPath = "/" + string.Join("/", folders);
         }
     }
 
