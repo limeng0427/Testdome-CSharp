4eb475f268b5c3eb2dd0d4d036663ea145b41e20
Liam <limeng0427@users.noreply.github.com>
2019-11-02 14:14:04 +1300

    Update SortedSearch.cs

--- CSharpTestDome/CSharpTestDome/SortedSearch.cs
+++ CSharpTestDome/CSharpTestDome/SortedSearch.cs
@@ -10,45 +10,43 @@ using System;
 
 public class SortedSearch
 {
-    // Runs recursively
-    private static int FindMid(int[] sortedArray, int left, int right, int lessThan)
-    {
-        var middle = left + (right - left) / 2; // = 0 + 1.5, since it's of type integer it will be 1
-
-        if (sortedArray[middle] == lessThan) // 3 < 4
-        {
-            // If duplicates are present it returns the position of the first element
-            while (middle - 1 > 0 && sortedArray[middle - 1] == lessThan) --middle;
-
-            return middle;
-        }
-
-        if (middle <= left)
-        {
-            // It happens if lessThan is not present in the array
-            while (middle <= right && sortedArray[middle] < lessThan) ++middle;
-
-            return middle;
-        }
-
-        if (sortedArray[middle] < lessThan)
-        {
-            left = middle;  // ignore left half
-        }
-        else
-        {
-            right = middle;  // ignore right half
-        }
-
-        return FindMid(sortedArray, left, right, lessThan);
-    }
-
     public static int CountNumbers(int[] sortedArray, int lessThan)
     {
-        var left = 0;
-        var right = sortedArray.Length - 1; // = 3
-
-        return FindMid(sortedArray, left, right, lessThan);
+        int start = 0;
+        int end = sortedArray.Length - 1;
+        int mid = 0;
+        while(start < end)
+        {
+            mid = (start + end)/2;
+            if (sortedArray[mid] > lessThan)
+            {
+                end = mid - 1;
+                if (start > end)
+                    end = start;
+            }
+            else if (sortedArray[mid] == lessThan)
+            {
+                return mid;
+            }
+            else
+            {
+                start = mid + 1;
+                if (start > sortedArray.Length - 1)
+                    start = sortedArray.Length - 1;
+            }
+        };
+        if(start == end)
+        {
+            if(sortedArray[start] < lessThan)
+            {
+                return start +1;
+            }
+            else
+            {
+                return start;
+            }
+        }
+        return 0;
     }
 
     public static void Main(string[] args)
